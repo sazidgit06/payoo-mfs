e6f38fe5c32115ade58c10845de74d7bbb837520
Sazid <sazidtec06@gmail.com>
2025-03-07 16:51:51 +0600

    add money and cash out btn complete

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+// add money
 document.getElementById('add-money').addEventListener('click',function(event){
     event.preventDefault();
     const amount = document.getElementById('amount-id').value;
@@ -12,3 +13,50 @@ document.getElementById('add-money').addEventListener('click',function(event){
         alert('pin error');
     }
 });
+
+// cash out
+document.getElementById('cash-out').addEventListener('click',function(event){
+    event.preventDefault();
+    const amountString = document.getElementById('amount-out-id').value;
+    const pinString = document.getElementById('pin-id-out').value;
+    const presentAmountString = document.getElementById('stat-value').innerText;
+    if(pinString === '1122'){
+        const amountNumber = parseFloat(amountString);
+        const presentAmountNumber = parseFloat(presentAmountString);
+        const newBalance = Math.abs(amountNumber - presentAmountNumber);
+        document.getElementById('stat-value').innerText = newBalance;
+    }else{
+        alert('Cash out fail please try again!');
+    }
+});
+
+// features adding
+
+document.getElementById('cash-out-id').addEventListener('click', function(){
+    document.getElementById('cash-out-form').classList.remove('hidden');
+    document.getElementById('add-money-form-id').classList.add('hidden');
+
+});
+document.getElementById('add-money-button-id').addEventListener('click', function(){
+    document.getElementById('add-money-form-id').classList.remove('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
