16f0270f7f6b3f7b77aa96a2b7352aab485cc1dc
Sazid <sazidtec06@gmail.com>
2025-03-07 12:58:13 +0600

    add money processing done

--- js/home.js
+++ js/home.js
@@ -1,1 +1,14 @@
-console.log('home added')
+document.getElementById('add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    const amount = document.getElementById('amount-id').value;
+    const pinNumber = document.getElementById('pin-id').value;
+    const statValue = document.getElementById('stat-value').innerText;
+    if(pinNumber === '1122'){
+        const amountNumber = parseFloat(amount);
+        const statValueNumber = parseFloat(statValue);
+        const newBalance = amountNumber + statValueNumber;
+        document.getElementById('stat-value').innerText = newBalance;
+    }else{
+        alert('pin error');
+    }
+});
